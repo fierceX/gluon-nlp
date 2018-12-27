30bc92c461098db564bbfa416382604ef5639483
fiercex <fiercex@outlook.com>
2018-12-27 17:50:35 +0800

    Organize code

--- scripts/bert_SQuAD/finetune_squad.py
+++ scripts/bert_SQuAD/finetune_squad.py
@@ -1,8 +1,45 @@
+"""
+SQuAD with Bidirectional Encoder Representations from Transformers
+
+=========================================================================================
+
+This example shows how to implement finetune a model with pre-trained BERT parameters for
+SQuAD, with Gluon NLP Toolkit.
+
+@article{devlin2018bert,
+  title={BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding},
+  author={Devlin, Jacob and Chang, Ming-Wei and Lee, Kenton and Toutanova, Kristina},
+  journal={arXiv preprint arXiv:1810.04805},
+  year={2018}
+}
+"""
+
+# coding: utf-8
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# pylint:disable=redefined-outer-name,logging-format-interpolation
+
 import argparse
 import collections
 import json
 import logging
 import random
+import time
 
 import gluonnlp as nlp
 import mxnet as mx
@@ -136,6 +173,7 @@ def Train():
 
     for epoch_id in range(epochs):
         step_loss = 0.0
+        tic = time.time()
         for batch_id, data in enumerate(train_dataloader):
 
             step_num += 1
@@ -164,9 +202,11 @@ def Train():
             step_loss += ls.asscalar()
 
             if (batch_id+1) % log_interval == 0:
-                logging.info('Epoch: %d, Batch: %d/%d, Loss=%.4f, lr=%.7f' %
+                toc = time.time()
+                logging.info('Epoch: %d, Batch: %d/%d, Loss=%.4f, lr=%.7f Time cost=%.1f' %
                              (epoch_id, batch_id, len(train_dataloader),
-                              step_loss/(log_interval), trainer.learning_rate))
+                              step_loss/(log_interval), trainer.learning_rate, toc-tic))
+                tic = time.time()
                 step_loss = 0.0
 
 
